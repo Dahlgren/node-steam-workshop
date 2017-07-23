f5dab37bc57be40bfeb701347a7156e389182735
Björn Dahlgren <bjorn@dahlgren.at>
2017-07-24 01:19:23 +0200

    Format all code with standard

--- src/workshop.js
+++ src/workshop.js
@@ -1,14 +1,14 @@
-var async = require('async');
-var fs = require('fs');
-var path = require('path');
-var request = require('request');
+var async = require('async')
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
 
-var FILE_URL = "http://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v0001/";
-var COLLECTION_URL = "http://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v0001/";
+var FILE_URL = 'http://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v0001/'
+var COLLECTION_URL = 'http://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v0001/'
 
 var SteamWorkshop = function (folder) {
-  this.folder = folder;
-};
+  this.folder = folder
+}
 
 /* Prepare request payload */
 
@@ -16,152 +16,150 @@ SteamWorkshop.prepareCollectionData = function (ids) {
   return {
     format: 'json',
     collectioncount: ids.length,
-    publishedfileids: ids,
-  };
-};
+    publishedfileids: ids
+  }
+}
 
 SteamWorkshop.prepareFilesData = function (ids) {
   return {
     format: 'json',
     itemcount: ids.length,
-    publishedfileids: ids,
-  };
-};
+    publishedfileids: ids
+  }
+}
 
 /* Load data from Steam Workshop API */
 
 SteamWorkshop.prototype.loadCollectionData = function (requestData, cb) {
   request.post(COLLECTION_URL, {form: requestData}, function (err, resp, body) {
     if (err) {
-      cb(err);
-      return;
+      return cb(err)
     }
 
-    var data = null;
+    var data = null
     try {
-      data = JSON.parse(body);
-    } catch(e) {
-      return cb(new Error("Error parsing response from Steam Workshop"));
+      data = JSON.parse(body)
+    } catch (e) {
+      return cb(new Error('Error parsing response from Steam Workshop'))
     }
 
     if (!data || !data.response || !data.response.collectiondetails) {
-      return cb(new Error("No data found"));
+      return cb(new Error('No data found'))
     }
 
     var fileIds = data.response.collectiondetails.map(function (collection) {
       if (!collection.children) {
-        return [];
+        return []
       }
 
       return collection.children.map(function (file) {
-        return file.publishedfileid;
-      });
-    }).reduce(function(a, b) {
-      return a.concat(b);
-    });
-    cb(null, fileIds);
-  });
-};
+        return file.publishedfileid
+      })
+    }).reduce(function (a, b) {
+      return a.concat(b)
+    })
+    cb(null, fileIds)
+  })
+}
 
 SteamWorkshop.prototype.loadFilesData = function (requestData, cb) {
   request.post(FILE_URL, {form: requestData}, function (err, resp, body) {
     if (err) {
-      cb(err);
-      return;
+      return cb(err)
     }
 
-    var data = null;
+    var data = null
     try {
-      data = JSON.parse(body);
-    } catch(e) {
-      return cb(new Error("Error parsing response from Steam Workshop"));
+      data = JSON.parse(body)
+    } catch (e) {
+      return cb(new Error('Error parsing response from Steam Workshop'))
     }
 
     if (!data || !data.response || !data.response.publishedfiledetails) {
-      return cb(new Error("No data found"));
+      return cb(new Error('No data found'))
     }
 
-    var files = data.response.publishedfiledetails;
-    cb(null, files);
-  });
-};
+    var files = data.response.publishedfiledetails
+    cb(null, files)
+  })
+}
 
 /* Actual file download */
 
 SteamWorkshop.prototype.saveFileToDisk = function (file, folder, cb) {
   var f = fs.createWriteStream(path.join(folder, file.filename))
-    .on('error', function(err) {
-      cb(err);
+    .on('error', function (err) {
+      cb(err)
     })
-    .on('finish', function() {
-      cb();
-    });
+    .on('finish', function () {
+      cb()
+    })
 
   request(file.file_url)
-    .on('error', function(err) {
-      cb(err);
+    .on('error', function (err) {
+      cb(err)
     })
-    .pipe(f);
-};
+    .pipe(f)
+}
 
 SteamWorkshop.prototype.saveFilesToDisk = function (files, folder, cb) {
-  var self = this;
+  var self = this
 
   async.map(files, function (file, done) {
     if (!file.file_url) {
-      done();
+      done()
     } else {
-      self.saveFileToDisk(file, folder, done);
+      self.saveFileToDisk(file, folder, done)
     }
   }, function (err, results) {
-    cb(err);
-  });
-};
+    cb(err)
+  })
+}
 
 /*
  * Download multiple files from Steam Workshop
  */
 SteamWorkshop.prototype.downloadFiles = function (ids, cb) {
-  var self = this;
+  var self = this
 
-  self.loadFilesData(SteamWorkshop.prepareFilesData(ids), function(err, files) {
+  self.loadFilesData(SteamWorkshop.prepareFilesData(ids), function (err, files) {
     if (err) {
-      cb(err);
+      cb(err)
     } else {
       self.saveFilesToDisk(files, self.folder, function (err) {
-        cb(err, files);
-      });
+        cb(err, files)
+      })
     }
-  });
-};
+  })
+}
 
 /*
  * Download single file from Steam Workshop
  */
 SteamWorkshop.prototype.downloadFile = function (id, cb) {
-  this.downloadFiles([id], cb);
-};
+  this.downloadFiles([id], cb)
+}
 
 /*
  * Download multiple collections from Steam Workshop
  */
 SteamWorkshop.prototype.downloadCollections = function (ids, cb) {
-  var self = this;
+  var self = this
 
-  self.loadCollectionData(SteamWorkshop.prepareCollectionData(ids), function(err, fileIds) {
+  self.loadCollectionData(SteamWorkshop.prepareCollectionData(ids), function (err, fileIds) {
     if (err) {
-      cb(err);
+      cb(err)
     } else {
-      self.downloadFiles(fileIds, cb);
+      self.downloadFiles(fileIds, cb)
     }
-  });
-};
+  })
+}
 
 /*
  * Download single collection from Steam Workshop
  */
 SteamWorkshop.prototype.downloadCollection = function (id, cb) {
-  this.downloadCollections([id], cb);
-};
+  this.downloadCollections([id], cb)
+}
 
-module.exports = SteamWorkshop;
+module.exports = SteamWorkshop
--- test/workshop.js
+++ test/workshop.js
@@ -1,34 +1,30 @@
-var should = require('should');
-var SteamWorkshop = require('../src/workshop');
+require('should')
+var SteamWorkshop = require('../src/workshop')
 
 describe('SteamWorkshop', function () {
   describe('prepareCollectionData', function () {
-    it('should set single id correctly', function (done) {
-      var data = SteamWorkshop.prepareCollectionData([123]);
-      data.publishedfileids.should.eql([123]);
-      data.collectioncount.should.eql(1);
-      done();
-    });
+    it('should set single id correctly', function () {
+      var data = SteamWorkshop.prepareCollectionData([123])
+      data.publishedfileids.should.eql([123])
+      data.collectioncount.should.eql(1)
+    })
 
-    it('should set multiple ids correctly', function (done) {
-      var data = SteamWorkshop.prepareCollectionData([123, 456]);
-      data.publishedfileids.should.eql([123, 456]);
-      data.collectioncount.should.eql(2);
-      done();
-    });
-  });
+    it('should set multiple ids correctly', function () {
+      var data = SteamWorkshop.prepareCollectionData([123, 456])
+      data.publishedfileids.should.eql([123, 456])
+      data.collectioncount.should.eql(2)
+    })
+  })
 
-  describe('prepareFileData', function() {
-    it('should set single id correctly', function (done) {
-      SteamWorkshop.prepareFilesData([123]).publishedfileids.should.eql([123]);
-      done();
-    });
+  describe('prepareFileData', function () {
+    it('should set single id correctly', function () {
+      SteamWorkshop.prepareFilesData([123]).publishedfileids.should.eql([123])
+    })
 
-    it('should set multiple ids correctly', function (done) {
-      var data = SteamWorkshop.prepareFilesData([123, 456]);
-      data.publishedfileids.should.eql([123, 456]);
-      data.itemcount.should.eql(2);
-      done();
-    });
-  });
-});
+    it('should set multiple ids correctly', function () {
+      var data = SteamWorkshop.prepareFilesData([123, 456])
+      data.publishedfileids.should.eql([123, 456])
+      data.itemcount.should.eql(2)
+    })
+  })
+})
