d0c927fc25c9cb9aba58233e65481caa3ab823cc
Björn Dahlgren <bjorn@dahlgren.at>
2020-04-05 13:40:27 +0200

    Remove usage of should in favor of assert

--- src/workshop.test.js
+++ src/workshop.test.js
@@ -1,57 +1,58 @@
-require('should')
+var assert = require('assert').strict
 var SteamWorkshop = require('./workshop')
 
 describe('SteamWorkshop', function () {
   describe('ensureArray', function () {
     it('should change number to array of number', function () {
       var arr = SteamWorkshop.ensureArray(123)
-      arr.should.eql([123])
+      assert.deepEqual(arr, [123])
     })
 
     it('should change string to array of string', function () {
       var arr = SteamWorkshop.ensureArray('123')
-      arr.should.eql(['123'])
+      assert.deepEqual(arr, ['123'])
     })
 
     it('should retain array of number', function () {
       var arr = SteamWorkshop.ensureArray([123])
-      arr.should.eql([123])
+      assert.deepEqual(arr, [123])
     })
 
     it('should retain array of numbers', function () {
       var arr = SteamWorkshop.ensureArray([123, 456])
-      arr.should.eql([123, 456])
+      assert.deepEqual(arr, [123, 456])
     })
 
     it('should retain array of string', function () {
       var arr = SteamWorkshop.ensureArray(['123', '456'])
-      arr.should.eql(['123', '456'])
+      assert.deepEqual(arr, ['123', '456'])
     })
   })
 
   describe('prepareCollectionData', function () {
     it('should set single id correctly', function () {
       var data = SteamWorkshop.prepareCollectionData([123])
-      data.publishedfileids.should.eql([123])
-      data.collectioncount.should.eql(1)
+      assert.deepEqual(data.publishedfileids, [123])
+      assert.deepEqual(data.collectioncount, 1)
     })
 
     it('should set multiple ids correctly', function () {
       var data = SteamWorkshop.prepareCollectionData([123, 456])
-      data.publishedfileids.should.eql([123, 456])
-      data.collectioncount.should.eql(2)
+      assert.deepEqual(data.publishedfileids, [123, 456])
+      assert.deepEqual(data.collectioncount, 2)
     })
   })
 
   describe('prepareFileData', function () {
     it('should set single id correctly', function () {
-      SteamWorkshop.prepareFilesData([123]).publishedfileids.should.eql([123])
+      var data = SteamWorkshop.prepareFilesData([123])
+      assert.deepEqual(data.publishedfileids, [123])
     })
 
     it('should set multiple ids correctly', function () {
       var data = SteamWorkshop.prepareFilesData([123, 456])
-      data.publishedfileids.should.eql([123, 456])
-      data.itemcount.should.eql(2)
+      assert.deepEqual(data.publishedfileids, [123, 456])
+      assert.deepEqual(data.itemcount, 2)
     })
   })
 })
